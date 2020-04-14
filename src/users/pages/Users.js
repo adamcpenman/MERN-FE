1ab23cb5a181f6b96b70fa58f30df31a98ea9935
Adam <adamcpenman@gmail.com>
2020-04-14 10:24:57 -0500

    Users and User Item components

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,7 +3,15 @@ import React from 'react';
 import UserList from '../components/UsersList';
 
 const Users = () => {
-  const USERS = [{ id: 'u1', name: 'john doe', places: 3 }];
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'adam penman',
+      image:
+        'https://avatars0.githubusercontent.com/u/50333588?s=460&u=23186af7f5f0c0ce5595b0b1e3f6e4ae536d4808&v=4',
+      places: 3,
+    },
+  ];
 
   return <UserList items={USERS} />;
 };
