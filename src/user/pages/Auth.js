c2eef59448cc5f0a3f5885155ac2c6c0c750baea
Adam <adamcpenman@gmail.com>
2020-04-28 13:23:58 -0500

    user image uploaded

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -40,6 +41,7 @@ const Auth = () => {
         {
           ...formState.inputs,
           name: undefined,
+          image: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -49,6 +51,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
+            isValid: false,
+          },
+          image: {
+            value: null,
             isValid: false,
           },
         },
@@ -78,17 +84,15 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            'Content-Type': 'application/json',
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
@@ -101,7 +105,7 @@ const Auth = () => {
       <ErrorModal error={error} onClear={clearError} />
       <Card className='authentication'>
         {isLoading && <LoadingSpinner asOverlay />}
-        {isLoginMode ? <h2>Login Required</h2> : <h2>Signup Below</h2>}
+        <h2>Login Required</h2>
         <hr />
         <form onSubmit={authSubmitHandler}>
           {!isLoginMode && (
@@ -111,17 +115,25 @@ const Auth = () => {
               type='text'
               label='Your Name'
               validators={[VALIDATOR_REQUIRE()]}
-              errorTExt='Please enter a name'
+              errorText='Please enter a name.'
               onInput={inputHandler}
+            />
+          )}
+          {!isLoginMode && (
+            <ImageUpload
+              center
+              id='image'
+              onInput={inputHandler}
+              errorText={'Please provide an image'}
             />
           )}
           <Input
             element='input'
             id='email'
             type='email'
-            label='Email'
+            label='E-Mail'
             validators={[VALIDATOR_EMAIL()]}
-            errorText='Please enter a valid email address'
+            errorText='Please enter a valid email address.'
             onInput={inputHandler}
           />
           <Input
@@ -130,15 +142,15 @@ const Auth = () => {
             type='password'
             label='Password'
             validators={[VALIDATOR_MINLENGTH(6)]}
-            errorText='Please enter a valid password'
+            errorText='Please enter a valid password, at least 6 characters.'
             onInput={inputHandler}
           />
           <Button type='submit' disabled={!formState.isValid}>
-            {isLoginMode ? 'Login' : 'Signup'}
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
           </Button>
         </form>
         <Button inverse onClick={switchModeHandler}>
-          Switch to {!isLoginMode ? 'Login' : 'Signup'}
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
       </Card>
     </React.Fragment>
